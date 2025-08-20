f6fd886d86e5a3e01cd21a018e06a27bca481a52
janko33bd <janko33bd@gmail.com>
2025-08-20 16:06:44 +0200

    skeleton application

---
+++ tribe/src/main/java/jb/devel/tribe/TribeApplication.java
@@ -0,0 +1,13 @@
+package jb.devel.tribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TribeApplication.class, args);
+	}
+
+}
---
+++ tribe/src/test/java/jb/devel/tribe/TribeApplicationTests.java
@@ -0,0 +1,13 @@
+package jb.devel.tribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
